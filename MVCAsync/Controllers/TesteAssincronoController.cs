1abaeceefaf0b7c3671d6a0fc22b8b6c632053f2
Edgar Júnior <edgar.net.9@gmail.com>
2016-05-28 11:33:53 -0300

    Adição da Controller

---
+++ MVCAsync/Controllers/TesteAssincronoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAsync.Controllers
+{
+    public class TesteAssincronoController : Controller
+    {
+        // GET: TesteAssincrono
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sincrono()
+        {
+
+            return View("Index");
+        }
+
+        public ActionResult Assincrono()
+        {
+
+            return View("Index");
+        }
+    }
+}
