a47816e218d774d751ef91a59a6f3c9ca7b00776
Edgar Júnior <edgar.net.9@gmail.com>
2016-05-28 21:21:49 -0300

    Application

---
+++ Application/TesteAsync.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class TesteAsync
+    {
+        public int Processar1()
+        {
+            Method1();
+            return 1;
+        }
+
+        public int Processar2()
+        {
+            Method2();
+            return 2;
+        }
+
+        public int Processar3()
+        {
+            Method3();
+            return 3;
+        }
+
+        public async Task<int> ProcessarAsync1()
+        {
+            await new Task(Method1);
+            return 1;
+        }
+        public async Task<int> ProcessarAsync2()
+        {
+            await new Task(Method2);
+            return 1;
+        }
+        public async Task<int> ProcessarAsync3()
+        {
+            await new Task(Method3);
+            return 1;
+        }
+
+        private void Method1()
+        {
+            Thread.Sleep(10000);
+        }
+
+        private void Method2()
+        {
+            Thread.Sleep(10000);
+        }
+
+        private void Method3()
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
--- MVCAsync/Controllers/TesteAssincronoController.cs
+++ MVCAsync/Controllers/TesteAssincronoController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Application;
 
 namespace MVCAsync.Controllers
 {
@@ -12,13 +15,30 @@ namespace MVCAsync.Controllers
 
         public ActionResult Sincrono()
         {
-
+            ViewBag.Inicio = DateTime.Now;
+            TesteAsync t = new TesteAsync();
+            var x = t.Processar1();
+            var y = t.Processar2();
+            var z = t.Processar3();
+            ViewBag.Fim = DateTime.Now;
             return View("Index");
         }
 
-        public ActionResult Assincrono()
+        public async Task<ActionResult> Assincrono()
         {
+            ViewBag.Inicio = DateTime.Now;
+            TesteAsync t = new TesteAsync();
+            var x = t.ProcessarAsync1();
+            var y = t.ProcessarAsync2();
+            var z = t.ProcessarAsync3();
 
+            await Task.WhenAll(x, y, z);
+
+            var a = x.Result;
+            var b = y.Result;
+            var c = z.Result;
+
+            ViewBag.Fim = DateTime.Now;
             return View("Index");
         }
     }
